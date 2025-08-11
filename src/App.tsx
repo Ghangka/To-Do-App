0f4e04c26292ec4f2f5110c0d9a409189c760cc4
Ghangka <ghangka.s@gmail.com>
2025-08-11 17:25:44 -0400

    created initial project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Add from "./components/add";
+
+function App() {
+  return (
+    <>
+      <div id="components">
+        <h1>To Do App</h1>
+        <Add></Add>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/add.tsx
@@ -0,0 +1,10 @@
+import "./add.css";
+
+export default function Add() {
+  return (
+    <div id="add">
+      <input type="text" placeholder={"Item name"} />
+      <button>Add Item ➕</button>
+    </div>
+  );
+}
